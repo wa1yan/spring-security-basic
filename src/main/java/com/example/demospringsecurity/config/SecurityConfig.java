e7fba077a10c8c5cddbabe78f001b66661c70959
Wai Yan Htet <waiyanhtet@startinpoint.com>
2021-12-08 10:26:22 +0630

    spring security basic with configuration

---
+++ src/main/java/com/example/demospringsecurity/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.demospringsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("william")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeHttpRequests().anyRequest().authenticated();
+    }
+}
---
+++ src/main/java/com/example/demospringsecurity/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demospringsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(){
+        return "Hello from controller";
+    }
+}
